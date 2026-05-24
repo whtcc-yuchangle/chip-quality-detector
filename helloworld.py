f89f19382523c8d3b32b803876ec84c7100b5345
whtcc-yuchangle <1043306031@qq.com>
2026-05-24 21:25:41 +0800

    为 helloworld.py 添加中英文双语注释

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+
+# This is a simple Hello World program | 这是一个简单的 Hello World 程序
+# The print() function outputs text to the console | print() 函数将文本输出到控制台
+# Lines starting with # are comments — they are ignored by Python and won't be executed as code
+# 以 # 号开头的行是注释，Python 会忽略它们，不会作为代码被执行
+print("Hello World!")
